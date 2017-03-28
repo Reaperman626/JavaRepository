612b74970207a8e1bbc1bf9bd9b6908713ce77d1
Reaperman626 <bm122190@students.davenport.k12.ia.us>
2017-03-28 12:36:59 -0500

    update

---
+++ Final Project/src/hello/HelloWorld.java
@@ -0,0 +1,19 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Look Mom Im programming, by the way Mr. Brosius is not a very good speller.");
+		System.out.println("l");
+		System.out.println("l");
+		System.out.println("l");
+		System.out.println("--------");
+		System.out.println("l      l");
+		System.out.println("l      l");
+		
+	}
+
+}
