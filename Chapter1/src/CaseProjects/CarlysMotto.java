fd8e40a89775fc59ffb309710ace9d0145e3d2b0
Reaperman626 <bm122190@students.davenport.k12.ia.us>
2017-03-28 12:58:29 -0500

    update

---
+++ Chapter1/src/CaseProjects/CarlysMotto.java
@@ -0,0 +1,13 @@
+package CaseProjects;
+
+public class CarlysMotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("***********************************************");
+		System.out.println("*Carly's Makes the food that makes it a party!*");
+		System.out.println("*    Sammy's Makes it fun in the sun!         *");
+		System.out.println("***********************************************");
+	}
+
+}
