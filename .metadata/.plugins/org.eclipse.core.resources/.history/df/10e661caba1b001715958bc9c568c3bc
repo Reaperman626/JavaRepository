45b943702c409d5399b6f134cd2c5b7ebd5bf34c
Reaperman626 <bm122190@students.davenport.k12.ia.us>
2017-04-07 13:13:15 -0500

    update

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/df/10e661caba1b001715958bc9c568c3bc
@@ -0,0 +1,10 @@
+package methodConstruction;
+
+public class page134Modified {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Chapter3/src/methodConstruction/page132.java
+++ Chapter3/src/methodConstruction/page132.java
@@ -4,7 +4,19 @@ public class page132 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		double salary = 350.00;
+		double startingWage = 720.00;
+		System.out.println("Demonstrating some raises");
+		predictRaise(400.00);
+		predictRaise(salary);
+		predictRaise(startingWage);
 	}
-
+public static void predictRaise(double salary)
+{
+	double newSalary;
+	final double RAISE_RATE = 1.50;
+	newSalary = salary * RAISE_RATE;
+	System.out.println("Current salary: " + salary + "  After raise: " + newSalary);
+	
+}
 }
