3f97e20c343b9e9b724dbb8f7303415bc9c06c88
Reaperman626 <bm122190@students.davenport.k12.ia.us>
2017-04-12 10:39:55 -0500

    update

--- Chapter3/src/youDoIt/SpaService.java
+++ Chapter3/src/youDoIt/SpaService.java
@@ -1,10 +1,21 @@
 package youDoIt;
 
 public class SpaService {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		private String serviceDescription;
+		private double price;
+	public void setServiceDescription(String service)
+	{
+		serviceDescription = service;
 	}
-
+	public void setPrice(double pr)
+	{
+		price = pr;
+	}
+public String getServiceDescription()
+{
+	return serviceDescription;
 }
+public double getPrice()
+{
+	return price;
+}}
